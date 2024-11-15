4bb104486c6662293ac56c9c618c88faa7eba071
Zanoza <piroshovs@gmail.com>
2024-11-16 01:49:15 +0300

    Update square.py

--- square.py
+++ square.py
@@ -1,7 +1,14 @@
-
 def area(a):
+    # Проверка положительного значения
+    if a <= 0:
+        raise ValueError("Side length must be positive")
+    
     return a * a
 
 
 def perimeter(a):
+    # Проверка положительного значения
+    if a <= 0:
+        raise ValueError("Side length must be positive")
+    
     return 4 * a
