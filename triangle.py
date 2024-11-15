3dac104d639721e5d764a5d695368c479574a7e0
Zanoza <piroshovs@gmail.com>
2024-11-16 01:54:46 +0300

    Update triangle.py

--- triangle.py
+++ triangle.py
@@ -2,7 +2,7 @@ def area(a, b, c):
     # Проверка корректности сторон треугольника
     if not (a + b > c and a + c > b and b + c > a):
         raise ValueError("Invalid side lengths for a triangle")
-    if a <= 0 or b <= 0 or c <= 0:
+    if a < 0 or b < 0 or c < 0:
         raise ValueError("Side lengths must be positive")
     
     p = (a + b + c) / 2
@@ -13,7 +13,7 @@ def perimeter(a, b, c):
     # Проверка корректности сторон треугольника
     if not (a + b > c and a + c > b and b + c > a):
         raise ValueError("Invalid side lengths for a triangle")
-    if a <= 0 or b <= 0 or c <= 0:
+    if a < 0 or b < 0 or c < 0:
         raise ValueError("Side lengths must be positive")
 
     return a + b + c
