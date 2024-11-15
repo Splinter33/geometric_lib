51e7a59ad51e1357818c29dfd30675380dc3839e
Zanoza <piroshovs@gmail.com>
2024-11-16 01:49:33 +0300

    Update circle.py

--- circle.py
+++ circle.py
@@ -1,9 +1,16 @@
 import math
 
-
 def area(r):
+    # Проверка положительного значения радиуса
+    if r <= 0:
+        raise ValueError("Radius must be positive")
+    
     return math.pi * r * r
 
 
 def perimeter(r):
+    # Проверка положительного значения радиуса
+    if r <= 0:
+        raise ValueError("Radius must be positive")
+    
     return 2 * math.pi * r
